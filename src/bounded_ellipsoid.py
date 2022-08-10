1ec910450dc051ac7f4621ff00ce571989a59a8e
halt <halt9000@protonmail.com>
2022-08-10 15:58:51 +0300

    better axis projections approach

--- src/bounded_ellipsoid.py
+++ src/bounded_ellipsoid.py
@@ -1,43 +1,66 @@
 import numpy as np
+from math import sqrt
 
-def cos_vecs(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b) + 0.01)
+def proj_length(v, v_on):
+    on_norm = np.linalg.norm(v_on)
+    v_len = np.linalg.norm(v)
 
-def iter_foci(pt_average, pts):
+    projection_len = 0
+    rejection_len = 0
+    if on_norm > 0.01:
+        projection_len = np.dot(v, v_on) / on_norm
+
+        if v_len > abs(projection_len):
+            rejection_len = sqrt(v_len**2 - projection_len**2)
+
+    return projection_len, rejection_len
+
+def find_foci(arr_pts):
+    # not nessesary for foci calc, just additional animation
     _pts_search_animations = []
-    vec_foci = pt_average * 0
+
+    # shuffle to improve main axis search, can be optimized
+    pts = np.copy(arr_pts)
+    np.random.shuffle(pts)
 
     pts_len = len(pts)
-    for i in range(pts_len * 2):
-        idx = i % pts_len
-        pt_cur = pts[idx]
+    pt_average =  np.sum(pts, axis = 0) / pts_len
+    
+    vec_major = pt_average * 0
+    minor_max, major_max = 0, 0
+    
+    # may be improved with ovelapped pass, 
+    # when max calcs are started after delay when axis is less random
+    for pt_cur in pts:
         vec_cur = pt_cur - pt_average
+        unprojected_len = np.linalg.norm(vec_cur)
+        proj_len, rej_len = proj_length(vec_cur, vec_major)
 
-        cos = cos_vecs(vec_foci, vec_cur)
-        if cos < 0:
-            vec_cur *= -1
-        vec_foci += (vec_cur - vec_foci) / pts_len
-        
-        _pts_search_animations += [[pt_cur, np.copy(vec_foci)]]
+        if proj_len < 0:
+            vec_cur = -vec_cur            
+        vec_major += (vec_cur - vec_major) / pts_len
 
-    return vec_foci, _pts_search_animations
+        major_max = max(major_max, abs(proj_len))
+        minor_max = max(minor_max, rej_len)        
+        _pts_search_animations += [[pt_cur, np.copy(vec_major)]]
 
+    # if both very close, may happen
+    if major_max < minor_max:
+        major_max, minor_max = minor_max, major_max
 
-def find_ellipsoid(pts_cloud_orig):    
-    pts = np.copy(pts_cloud_orig)
-    np.random.shuffle(pts) # seems requred?
-    
-    pt_average =  np.sum(pts, axis = 0) / len(pts)
-    
-    vec_foci, _pts_search_animations = iter_foci(pt_average, pts)
-    # vec_foci, _pts_search_animations = iter_foci(pt_average, np.flip(np.copy(pts)))
-    # vec_foci = (vec_foci1 + vec_foci2) / 2
-    
+    vec_major_unit = vec_major / np.linalg.norm(vec_major)
+    vec_foci = vec_major_unit * sqrt( major_max**2 - minor_max**2)
+
     foci_1 = pt_average + vec_foci
     foci_2 = pt_average - vec_foci
 
+    return foci_1, foci_2, _pts_search_animations
+
+def find_ellipsoid(arr_pts):
+    foci_1, foci_2, _pts_search_animations = find_foci(arr_pts)
+    
     string_pro_calc = 0
-    for pt_cur in pts:
+    for pt_cur in arr_pts:
         cur_pt_radius = np.linalg.norm(pt_cur - foci_1) + np.linalg.norm(pt_cur - foci_2)
         string_pro_calc = max(string_pro_calc, cur_pt_radius)
 
--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ from visuals import PointsDrawer
 from bounded_ellipsoid import find_ellipsoid
 
 np.random.seed()
-pts = Sample.spheres(50, 2)
-# pts = Sample.sphere(200)
+# pts = Sample.spheres(50, 4)
+# pts = Sample.sphere(400)
 # pts = Sample.ball(200)
-# pts = Sample.cube(400)
+pts = Sample.cube(400)
 # pts = Sample.line(200)
 
-# pts = Sample.rnd_transform(pts)
+# unnessesary random transfrom of point cloud to spice it
+pts = Sample.rnd_transform(pts)
 
 foci_1, foci_2, elp_string, _pts_animation = find_ellipsoid(pts)
 print(foci_1, foci_2, elp_string)
