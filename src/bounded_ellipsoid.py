d3c343bdd858238371490f467c6b8b29cc7b0c65
halt <halt9000@protonmail.com>
2022-09-08 19:54:47 +0300

    code maintain

--- src/bounded_ellipsoid.py
+++ src/bounded_ellipsoid.py
@@ -1,5 +1,7 @@
-import numpy as np
 from math import sqrt
+
+import numpy as np
+
 
 def proj_length(v, v_on):
     on_norm = np.linalg.norm(v_on)
@@ -11,12 +13,13 @@ def proj_length(v, v_on):
         projection_len = np.dot(v, v_on) / on_norm
 
         if v_len > abs(projection_len):
-            rejection_len = sqrt(v_len**2 - projection_len**2)
+            rejection_len = sqrt(v_len ** 2 - projection_len ** 2)
 
     return projection_len, rejection_len
 
+
 def find_foci(arr_pts):
-    # not nessesary for foci calc, just additional animation
+    # not necessary for foci calc, just additional animation
     _pts_search_animations = []
 
     # shuffle to improve main axis search, can be optimized
@@ -24,23 +27,23 @@ def find_foci(arr_pts):
     np.random.shuffle(pts)
 
     pts_len = len(pts)
-    pt_average =  np.sum(pts, axis = 0) / pts_len
-    
+    pt_average = np.sum(pts, axis=0) / pts_len
+
     vec_major = pt_average * 0
     minor_max, major_max = 0, 0
-    
-    # may be improved with ovelapped pass, 
+
+    # may be improved with overlapped pass,
     # when max calcs are started after delay when axis is less random
     for pt_cur in pts:
         vec_cur = pt_cur - pt_average
         proj_len, rej_len = proj_length(vec_cur, vec_major)
 
         if proj_len < 0:
-            vec_cur = -vec_cur            
+            vec_cur = -vec_cur
         vec_major += (vec_cur - vec_major) / pts_len
 
         major_max = max(major_max, abs(proj_len))
-        minor_max = max(minor_max, rej_len)        
+        minor_max = max(minor_max, rej_len)
         _pts_search_animations += [[pt_cur, np.copy(vec_major)]]
 
     # if both very close, i.e. cloud is sphere, may happen
@@ -48,16 +51,17 @@ def find_foci(arr_pts):
         major_max, minor_max = minor_max, major_max
 
     vec_major_unit = vec_major / np.linalg.norm(vec_major)
-    vec_foci = vec_major_unit * sqrt( major_max**2 - minor_max**2)
+    vec_foci = vec_major_unit * sqrt(major_max ** 2 - minor_max ** 2)
 
     foci_1 = pt_average + vec_foci
     foci_2 = pt_average - vec_foci
 
     return foci_1, foci_2, _pts_search_animations
 
+
 def find_ellipsoid(arr_pts):
     foci_1, foci_2, _pts_search_animations = find_foci(arr_pts)
-    
+
     string_pro_calc = 0
     for pt_cur in arr_pts:
         cur_pt_radius = np.linalg.norm(pt_cur - foci_1) + np.linalg.norm(pt_cur - foci_2)
--- src/point_clouds_gen.py
+++ src/point_clouds_gen.py
@@ -1,5 +1,5 @@
 import numpy as np
-import typing
+
 
 class Sample:
     # np.random.seed()
@@ -8,42 +8,39 @@ class Sample:
         return np.random.rand(1, 3) * length + 0.5
 
     @staticmethod
-    def sphere(npoints, ndim=3):
-        vec = np.random.randn(ndim, npoints)
+    def sphere(pts_count, ndim=3):
+        vec = np.random.randn(ndim, pts_count)
         vec /= np.linalg.norm(vec, axis=0)
         return np.transpose(vec)
 
     @staticmethod
-    def ball(npoints, ndim=3):
-        vec = np.random.randn(ndim, npoints)    
+    def ball(pts_count, ndim=3):
+        vec = np.random.randn(ndim, pts_count)
         return np.transpose(vec)
 
     @staticmethod
-    def cube(npoints):
-        vec = np.random.rand(npoints, 3) - [0.5, 0.5, 0.5]
-        for n in range(npoints):
-             vec[n][np.random.randint(3)] = np.random.randint(2)-0.5
-        return  vec * 10
+    def cube(pts_count):
+        vec = np.random.rand(pts_count, 3) - [0.5, 0.5, 0.5]
+        for n in range(pts_count):
+            vec[n][np.random.randint(3)] = np.random.randint(2) - 0.5
+        return vec * 10
 
     @staticmethod
-    def spheres(npoints, count):
-        spheres = np.array([], dtype=np.float).reshape(0,3)
+    def spheres(pts_count, count):
+        spheres = np.array([], dtype=np.float).reshape(0, 3)
         for n in range(count):
-            sph = Sample.sphere(npoints) + Sample.rand_vec(20) - 5
+            sph = Sample.sphere(pts_count) + Sample.rand_vec(20) - 5
             spheres = np.vstack([spheres, sph])
         return spheres
 
     @staticmethod
-    def line(npoints):
+    def line(pts_count):
         a, b = Sample.rand_vec(10), Sample.rand_vec(10)
-        pts = a + np.transpose( np.transpose(b - a) * np.random.rand(npoints))
-        pts += np.random.rand(npoints, 3)
+        pts = a + np.transpose(np.transpose(b - a) * np.random.rand(pts_count))
+        pts += np.random.rand(pts_count, 3)
         return pts
 
     @staticmethod
     def rnd_transform(pts: np.array):
         mtr = np.random.rand(3, 3) * [[2, 0, 0], [0, 1, 1], [0, 1, 1]]
         return pts @ mtr
-
-        
-        
